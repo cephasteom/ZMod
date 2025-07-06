7c1b4f61f7d51967a309b35dcf8b12fbb41f793f
Peter Thomas <petethomasmusique@gmail.com>
2025-07-06 21:37:57 +0100

    refactor/class: tidy up

--- lib/main.ts
+++ lib/main.ts
@@ -1,25 +1,25 @@
 import { ZenModular } from "./ZenModular";
 
-const runButton = document.getElementById("run");
+const runButton = document.getElementById("start");
 const stopButton = document.getElementById("stop");
 const codeInput = document.getElementById("code") as HTMLTextAreaElement;
 
 const zm = new ZenModular()
 
-const run = () => zm.parse(codeInput.value).start()
+const start = () => zm.parse(codeInput.value).start()
 const stop = () => zm.clear()
 
-runButton?.addEventListener("click", run);
+runButton?.addEventListener("click", start);
 stopButton?.addEventListener("click", stop)
 
 codeInput?.addEventListener("keydown", (event) => {
     if (!(event.shiftKey && event.key === "Enter")) return
-    event.preventDefault(); // Prevent default behavior of new line
-    run();
+    event.preventDefault();
+    start();
 });
 
 codeInput?.addEventListener("keydown", (event) => {
     if (event.key !== "Escape") return;
-    event.preventDefault(); // Prevent default behavior of escape
+    event.preventDefault();
     stop();
 });
